aea10194af2045766c57439d4845305640a9bc8c
= <=>
2021-04-30 17:06:51 -0400

    change some of the comments

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ let cipher = str => {
 const userChoice = () => prompt("Please enter \'1\' to reverse a string and \'2\' to use the cipher.")
 
 
-// calls our userChoice() function to determine whether it should call the first callback function (reverse()) or the second callback function(cipher()).
+// calls a chooser function to determine whether it should call the first callback function or the second callback function.
 // calls the appropriate function.
 // returns a string.
 const stringAccesser = function(callback1, callback2, chooser = userchoice){
@@ -49,8 +49,8 @@ const stringAccesser = function(callback1, callback2, chooser = userchoice){
     // declare callback variable to hold the chosen callback function
     let callback;
     if(choice === '1'){
-        callback = callback1; //assigned to reverse()
-    } else callback = callback2; //assigned to cipher()
+        callback = callback1;
+    } else callback = callback2;
 
     // saves user input string to pass to our callback function
     let str = prompt("Please enter a string to change: ");
@@ -60,6 +60,7 @@ const stringAccesser = function(callback1, callback2, chooser = userchoice){
     return str
 }
 
-// logs our mutated string to the console
+// calls our stringAccesser function with reverse, cipher, and userChoice passed in as callback function arguments
 let finalString = stringAccesser(reverse, cipher, userChoice);
+// logs our mutated string to the console
 console.log(finalString);
